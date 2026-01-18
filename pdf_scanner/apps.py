937e675909558b2e08430475e6698ef56e099d60
Raghav0422 <kumarraghav561@gmail.com>
2026-01-18 15:22:10 +0530

    Setup is done and models is created

---
+++ pdf_scanner/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PdfScannerConfig(AppConfig):
+    name = 'pdf_scanner'
---
+++ pdf_scanner/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class PDFDocument(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='pdfs/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+    extracted_text = models.TextField(blank=True, null=True)  # To store text for faster searching
+
+    def __str__(self):
+        return self.name
